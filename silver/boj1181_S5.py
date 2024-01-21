1e3e2b24d9f23fd99a1dc187e54002d9de7dbf46
xray <minxray516@gmail.com>
2024-01-21 21:20:29 +0900

    백준 silver5 1181. 단어정렬 sort, key로 sort 하기

--- silver/boj1181_S5.py
+++ silver/boj1181_S5.py
@@ -1,15 +1,45 @@
-word_set = set()
-word_len = []
-num = int(input())
+# BOJ 1181. 단어 정렬 
 
+# # 필요한 변수 선언
+# word_set = set()
+# num = int(input())
+
+# # Set에 담가서 중복된 단어를 제거 
+# for i in range(num):
+#   word = str(input())
+#   word_set.add(word)
+
+# # 순서가 있는 시퀀스를 만들기 위해 list에 담음
+# word_list = list(word_set)
+# word_list = sorted(word_list)
+# word_list = sorted(word_list, key = len)
+
+# for word in word_list:
+#   print(word)
+
+# 방법 2 
+# 필요한 변수 선언
+word_set = set()
+num = int(input())
+sort_list = []
+
+# Set에 담가서 중복된 단어를 제거 
 for i in range(num):
   word = str(input())
   word_set.add(word)
 
+#(len(word), word) 를 리스트에 담아줄 것
 word_list = list(word_set)
+for word in word_list:
+  sort_list.append((len(word), word))
 
-for need_count in word_list:
-  word_len.append(len(need_count))
+# len(word) 기준으로 먼저 sort 하고 같을 땐 word 기준으로 sort한다
+sort_list.sort()
 
-for i in range(num):
-  print(word_len[i])
+# 뱉기
+
+for len_word, word in sort_list:
+  print(word)
+
+# for sorted_word in sort_list:
+#   print(sorted_word[1])
