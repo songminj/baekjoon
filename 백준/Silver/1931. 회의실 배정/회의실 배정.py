994c28fdaf4d1f0cddeb95a60155838eff3f41de
MJ <104004869+songminj@users.noreply.github.com>
2024-02-29 10:29:02 +0900

    [Silver I] Title: 회의실 배정, Time: 232 ms, Memory: 51900 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -4,14 +4,12 @@ import sys
 
 n = int(sys.stdin.readline())
 
-conv = [[] for _ in range(n)]
-for i in range(n):
-    start, end = map(int, input().split())
-    conv[i] = (start, end)
+conv = [tuple(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
 conv.sort(key= lambda x : (x[1], x[0]))
 # print(conv)
-cnt, e = 0, 0
-for i in range(n):
+cnt, e = 1, conv[0][1]
+for i in range(1, n):
     if conv[i][0] >= e:
         e = conv[i][1]
         cnt += 1
